80091a5f1d05aa7a8ebfebd1b8f54f590e40e3be
sotirisnik <sotirisnik@gmail.com>
2017-09-22 17:13:50 +0300

    Add files via upload

--- dqn.py
+++ dqn.py
@@ -23,7 +23,7 @@ import os
 import tensorflow as tf
 import keras.backend.tensorflow_backend as KTF
 
-def get_session(gpu_fraction=0.3):
+def get_session(gpu_fraction=0.7):
     '''Assume that you have 6GB of GPU memory and want to allocate ~2GB'''
 
     num_threads = os.environ.get('OMP_NUM_THREADS')
@@ -35,43 +35,21 @@ def get_session(gpu_fraction=0.3):
     else:
         return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
-KTF.set_session(get_session())
+def run_on_cpu():
 
+    config = tf.ConfigProto(
+        device_count = {'GPU': 0}
+    )
+    return tf.Session(config=config)
 
-# In[3]:
+#KTF.set_session(get_session())
+KTF.set_session( run_on_cpu() )
 
-
-env = gym.make( 'Breakout-v0')
-#env = gym.make( 'CartPole-v0' )
-
-
-# Get initial state
-
-# In[4]:
-
-
-observation = env.reset()
-
-
-# In[5]:
-
-
-observation.shape
-#210*160*3*4
-
-
-# In[6]:
-
-
-print( np.prod( observation.shape ) )
-
+env = gym.make( 'BreakoutDeterministic-v4')
 
 # Preprocess observation
 # Crop image - do not keep score
 # Grayscale image - reduce the rgb space to grayscale, save space
-
-# In[7]:
-
 
 import cv2
 import matplotlib.pyplot as plt
@@ -81,7 +59,7 @@ import matplotlib.pyplot as plt
 def to_grayscale( observation ):
     r, g, b = observation[:,:,0], observation[:,:,1], observation[:,:,2]
     ret = 0.299 * r + 0.587 * g + 0.114 * b
-    return ( ret )
+    return ( np.array( ret, dtype=np.uint8 ) )
 
 def preprocess_observation( observation ):
     res = cv2.resize( observation, (84,110) )
@@ -89,20 +67,22 @@ def preprocess_observation( observation ):
     grayscale = to_grayscale( crop )#cv2.cvtColor( crop, cv2.COLOR_BGR2GRAY )
     return ( grayscale )
 
-#plt.imshow( preprocess_observation( observation ), cmap='gray')
-
-#plt.show()
-
-preprocess_observation( observation ).shape
-
-
-# In[9]:
-
+"""
+def preprocess_observation( observation ):
+    res = cv2.resize( observation, (84,110) )#resize to 110x84
+    crop = res[18:110-8,:,:]#crop image
+    grayscale = to_grayscale( crop )
+    thresh, bn = cv2.threshold( grayscale, 80, 255, cv2.THRESH_BINARY )
+    #grayscale=cv2.cvtColor( crop, cv2.COLOR_BGR2GRAY )#apply grayscale
+    #grayscale = grayscale.astype( float ) / 255.0#normalize image
+    return ( np.array( bn / np.max(bn), dtype=np.uint8 ) )
+"""
 
 from keras.models import Sequential
-from keras.layers import Dense, Flatten, Activation
+from keras.layers import Dense, Flatten, Activation, Lambda
 from keras.layers.convolutional import Conv2D
 from keras.optimizers import RMSprop
+from keras import initializers
 
 # Build model
 
@@ -110,22 +90,25 @@ model = Sequential()
 
 #32 filters of kernel(3,3), stride=4, input shape must be in format row, col, channels
 #init='uniform',
-#model.add( Conv2D(32, (8,8), strides=(4,4), padding='same', input_shape=(84,84,4) ) )#deep mind
-model.add( Conv2D(16, (8,8), strides=(2,2), padding='same', input_shape=(84,84,4) ) )
+model.add( Conv2D(32, (8,8), strides=(4,4), padding='same', input_shape=(84,84,4) ) )#deep mind
+#model.add( Conv2D(16, (8,8), strides=(2,2), kernel_initializer=initializers.random_normal(stddev=0.01), padding='same', input_shape=(84,84,4) ) )
+
+model.add( Lambda(lambda x: x / 255.0, dtype='float32') )
+
 model.add( Activation( 'relu' ) )
 
-#model.add(Conv2D(64, (4,4), strides=(2,2), padding='same' ) )#deep min
-model.add(Conv2D(32, (4,4), strides=(2,2), padding='same' ) )
+model.add(Conv2D(64, (4,4), strides=(2,2), padding='same' ) )#deep min
+#model.add(Conv2D(32, (4,4), strides=(2,2), kernel_initializer=initializers.random_normal(stddev=0.01), padding='same' ) )
 model.add( Activation( 'relu' ) )
 
-model.add(Conv2D(64, (3,3), strides=(1,1), padding='same' ) )
+model.add(Conv2D(64, (3,3), strides=(1,1), kernel_initializer=initializers.random_normal(stddev=0.01), padding='same' ) )
 model.add( Activation( 'relu' ) )
 
 model.add(Flatten())
-model.add(Dense(512, activation='relu'))
-model.add(Dense(512, activation='relu'))
-model.add(Dense(512, activation='relu'))
-model.add( Dense( env.action_space.n, kernel_initializer='uniform', activation='relu' ) )
+model.add(Dense(512, kernel_initializer=initializers.random_normal(stddev=0.01), activation='relu'))
+model.add(Dense(256, kernel_initializer=initializers.random_normal(stddev=0.01), activation='relu'))
+model.add(Dense(128, kernel_initializer=initializers.random_normal(stddev=0.01), activation='relu'))
+model.add( Dense( env.action_space.n, kernel_initializer=initializers.random_normal(stddev=0.01), activation='relu' ) )
 #model.compile(RMSprop(), 'MSE')
 #model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
 learning_rate = 0.00025
@@ -147,6 +130,8 @@ alpha = 1#0.999999#00025
 
 max_reward = 0.0
 
+epoch = 0
+
 start_episode = 1
 
 epsilon = 1
@@ -156,9 +141,11 @@ exploration_steps = 1000000
 
 epsilon_discount = ( epsilon - epsilon_min ) / exploration_steps
 
-MAX_SIZE = 10000#capacity of deque
-MIN_MIN_SIZE = 5000#min size for replay
+MAX_SIZE = 40000#capacity of deque
+MIN_MIN_SIZE = 20000#min size for replay
 D = deque( maxlen=MAX_SIZE )#[]
+
+frames = 0
 
 def load_deque():
     global D
@@ -191,13 +178,14 @@ import pickle
 episodes = []
 rewards = []
 epsilons = []
+total_frames = []
 
 def save_train():
 
-    global episodes, rewards, epsilons
+    global episodes, rewards, epsilons, total_frames
 
     #save [episodes, rewards, epsilons ] to csv file
-    d = {'episode': episodes, 'reward': rewards, 'epsilon': epsilons}
+    d = {'episode': episodes, 'reward': rewards, 'epsilon': epsilons, 'total_frames': total_frames}
     df = pd.DataFrame(data=d, index=None)
 
     if not os.path.isfile('filename.csv'):
@@ -208,6 +196,7 @@ def save_train():
     episodes = []
     rewards = []
     epsilons = []
+    total_frames = []
 
     #save model to disk
     # serialize model to JSON
@@ -223,7 +212,7 @@ def save_train():
 
 def load_train():
 
-    global start_episode, epsilon
+    global start_episode, epsilon, frames
 
     #get last episode and epsilon
     if not os.path.isfile('filename.csv'):
@@ -232,19 +221,17 @@ def load_train():
         df = pd.read_csv( 'filename.csv')
 
         if len(df) == 0:
-            start_episode, epsilon = 1, 1
+            start_episode, epsilon, frames = 1, 1, 0
         else:
             epsilon = list( df['epsilon'].tail(1) )[0]
             start_episode = list( df['episode'].tail(1) )[0] + 1
+            frames = list( df['total_frames'].tail(1) )[0]
 
     if os.path.isfile('model_background.json'):
         load_dqn_model()
 
     #if os.path.isfile('mydeque.pkl'):
         #load_deque()
-
-def norm_data( data ):
-    return ( data.astype( float ) / 255.0 )
 
 load_train()
 
@@ -256,6 +243,11 @@ load_train()
 
 total_observe = 12000#total_episodes
 MIN_SIZE = 32
+
+observe_frame = 0
+
+def must_observe():
+    return ( observe_frame < MIN_MIN_SIZE )
 
 def replay( ):
 
@@ -273,9 +265,9 @@ def replay( ):
 
         observation, reward, done, new_observation, action = sample
 
-        y = model.predict( norm_data( observation.reshape(  ( 1, 84, 84, 4) ) ) )
+        y = model.predict( observation.reshape(  ( 1, 84, 84, 4) ) )
 
-        Q_next = model.predict( norm_data( new_observation.reshape(  ( 1, 84, 84, 4) ) ) )
+        Q_next = model.predict( new_observation.reshape(  ( 1, 84, 84, 4) ) )
 
         if done:
             y[0,action] += alpha * reward
@@ -298,11 +290,11 @@ def replay( ):
 
     del all_x, all_y
 
-    gc.collect()
-
 start = time.time()
 
-for episode in range( start_episode, total_observe+1 ):#3600*5):
+episode = start_episode
+
+while episode <= total_observe:#3600*5):
 
     observation = env.reset()
 
@@ -319,21 +311,41 @@ for episode in range( start_episode, total_observe+1 ):#3600*5):
 
     cur_lives = 5
 
+    step = 0
+
+    action = 0
+
+    steps = 0
+
     while True:
 
-        env.render()
+        #env.render()
 
         stack_observation = np.stack(recent_frames,axis=0)
 
+        if must_observe():
+            observe_frame += 1
+
+        if must_observe() == False:
+            steps += 1
+
+        #if step % 4 == 0:
         if random.uniform(0,1) < epsilon:
             action = env.action_space.sample()
         else:
-            Q = model.predict( norm_data( stack_observation.reshape(  ( 1, 84, 84, 4) ) ) )[0]
+            Q = model.predict( stack_observation.reshape(  ( 1, 84, 84, 4) ) )[0]
             action = np.argmax( Q )
+        #step = 0
 
         new_observation, reward, done, info = env.step( action )
 
         new_observation = preprocess_observation( new_observation )#apply preprocess
+
+        #plt.imshow( new_observation )
+
+        #plt.show(block=False)
+        #plt.pause(0.5)#.sleep(3)
+        #plt.close()
 
         next_recent_frames = recent_frames.copy()
         next_recent_frames.append( new_observation )
@@ -349,26 +361,38 @@ for episode in range( start_episode, total_observe+1 ):#3600*5):
 
         total_reward += reward
 
-        #replay()
+        replay()
 
         if done:
-            print(  str(episode) + "Game over!", end= ' ' ),
-            replay()
-            episodes.append( episode )
-            rewards.append( total_reward )
-            epsilons.append( epsilon )
+            #print(  str(episode) + "Game over!", end= ' ' ),
+            #replay()]]]
+            if must_observe() == False:
+                episodes.append( episode )
+                rewards.append( total_reward )
+                epsilons.append( epsilon )
             break
 
         observation = new_observation
 
         recent_frames.append( observation )
 
-        epsilon = max( epsilon_min, epsilon - epsilon_discount )
+        if must_observe() == False:
+            epsilon = max( epsilon_min, epsilon - epsilon_discount )
 
-    print( "episode " + str(episode) + " done with total reward := " + str(total_reward) )
+    if must_observe() == False:
+        frames += steps
+        total_frames.append( frames )
+        print( "Episode " + str(episode) + " | total reward := " + str(total_reward) + " | steps := " + str(steps) + " total frames := " + str(frames) )
+    else:
+        print( "Observe total frames := " + str(observe_frame) )
 
     if episode % 10 == 0 and episode > 1:
-        save_train()
+        if must_observe() == False:
+            save_train()
+            gc.collect()
+
+    if must_observe() == False:
+        episode += 1
 
 end = time.time()
 
